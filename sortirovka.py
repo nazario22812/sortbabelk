ad79570888324be7c6e1455930be743c421abcb1
nazario22812 <76971123+nazario22812@users.noreply.github.com>
2023-12-11 15:18:40 +0100

    Add files via upload

---
+++ sortirovka.py
@@ -0,0 +1,13 @@
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        cursor = arr[i]
+        pos = i
+        while pos > 0 and arr[pos - 1] > cursor:
+            arr[pos] = arr[pos - 1]
+            pos = pos -1 
+        arr[pos] = cursor 
+
+    return arr
+
+list = [6,5,3,1,8,7,2,4]
+print(insertion_sort(list))
